dde934355ecde500445d7e1629bbd83beac525ee
rahimkhowaja-4sight <Rahim.Khowaja@4sight-tech.com>
2020-06-28 14:40:53 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DegArena_WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
